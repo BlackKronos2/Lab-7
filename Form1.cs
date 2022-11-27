5c76303dd7c89370ecdb52d200f36d45a75a5075
BlackKronos2 <34336340+BlackKronos2@users.noreply.github.com>
2022-11-27 17:09:43 +0700

    Реализация движения по всем точкам массива

--- Form1.cs
+++ Form1.cs
@@ -13,31 +13,36 @@ namespace Lab_7
     public partial class Form1 : Form
     {
         public Render entity;
-        public PointF point1;
 
+        public PointF[] points;
+        public int index = 0;
         public Form1()
         {
             InitializeComponent();
 
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
-                
+
 
             timer1.Interval = 10;
             timer1.Tick += new EventHandler(Update);
             timer1.Start();
 
             entity = new Render(new Size(100, 100));
-            point1 = new PointF(300, 60);
+            points = new PointF[] {
+                new PointF(300, 60),
+                new PointF(280, 40),
+                new PointF(250, 70),
+            };
         }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
         private void Update(object sender, EventArgs e) {
-            entity.MoveToward(point1, 5);
-
+            if(index < points.Length)
+            if (entity.MoveToward(points[index], 1))
+                index++;
             Invalidate();
-
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
