d48e86e3df50e32de9d2c0b6b341aec61eeffd4e
Илья Масютин <i-masyutin@mail.ru>
2022-12-05 13:35:58 +0700

    Сделал чекбокс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,30 +31,59 @@ namespace Lab_7
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.Update);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кинуть кости ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(423, 325);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(200, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Не показывать анимацию";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,18 @@ namespace Lab_7
 {
     public partial class Form1 : Form
     {
-        public Render render;
+        int tics = 0;
+        bool button = false; 
+        static Random random1 = new Random((int)DateTime.Now.Ticks + 600);
+        static Random random2 = new Random((int)DateTime.Now.Ticks);
+
         public Render render1;
+        public Render render2;
+
         public Form1()
         {
             InitializeComponent();
-            timer1.Interval = 10;
+            timer1.Interval = 50;
             //timer1.Tick += new EventHandler(Update);
             timer1.Start();
         }
@@ -25,28 +31,151 @@ namespace Lab_7
         private void Form1_Load(object sender, EventArgs e)
         {
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            render1 = new Render(Resource1.b, new Size(100, 100));
+            render2 = new Render(Resource1.b, new Size(100, 100));
 
-            timer1.Interval = 10;
-            //timer1.Tick += new EventHandler(Update);
-            timer1.Start();
-            render = new Render(Resource1.b, new Size(100, 100));
-            render1 = new Render(Resource1.b2, new Size(100, 100));
-            render.position = new PointF(250, 100);
-            render1.position = new PointF(400, 100);
+            render1.position = new PointF(250, 100);
+            render2.position = new PointF(400, 100);
         }
+
+        //private void mixing1()
+        //{
+
+        //    const int N = 6;
+
+        //    int value = 1;
+        //    int value1 = random1.Next(1, 6); //получение случайного числа от 1 до 6 
+        //    switch (value1)
+        //    {
+        //        case 1:
+        //            render11.position = new PointF(250, 100);
+        //            break;
+        //        case 2:
+        //            render12.position = new PointF(250, 100);
+        //            break;
+        //        case 3:
+        //            render13.position = new PointF(250, 100);
+        //            break;
+        //        case 4:
+        //            render14.position = new PointF(250, 100);
+        //            break;
+        //        case 5:
+        //            render15.position = new PointF(250, 100);
+        //            break;
+        //        case 6:
+        //            render16.position = new PointF(250, 100);
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
+        //private void mixing2()
+        //{
+        //    int value2 = random2.Next(1, 6); //получение случайного числа от 1 до 6 
+        //    switch (value2)
+        //    {
+        //        case 1:
+        //            render21.position = new PointF(400, 100);
+        //            break;
+        //        case 2:
+        //            render22.position = new PointF(400, 100);
+        //            break;
+        //        case 3:
+        //            render23.position = new PointF(400, 100);
+        //            break;
+        //        case 4:
+        //            render24.position = new PointF(400, 100);
+        //            break;
+        //        case 5:
+        //            render25.position = new PointF(400, 100);
+        //            break;
+        //        case 6:
+        //            render26.position = new PointF(400, 100);
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+        private void Mixing()
+        {
+            int value1 = random1.Next(1, 7); //получение случайного числа от 1 до 6 
+            int value2 = random2.Next(1, 7); 
+            Image[] image = new Image[6] { Resource1.b, Resource1.b2, Resource1.b3, Resource1.b4, Resource1.b5, Resource1.b6 };
+            render1.sprite = image[value1 - 1];
+            render2.sprite = image[value2 - 1];
+        }
+
+        
 
         private void Update(object sender, EventArgs e) 
         {
-            //render.position.X += 1;
-            render.sprite = Resource1.b;
-            render1.sprite = Resource1.b2;
-            Invalidate();
+            if (button)
+            {
+                if (!checkBox1.Checked)
+                {
+                    if (tics <= 1000)
+                    {
+                        tics += timer1.Interval;
+                        Mixing();
+                    }
+                    else
+                    {
+                        tics = 0;
+                        button = false;
+                    }
+                    Invalidate();
+
+                }
+                if (checkBox1.Checked)
+                {
+                    if (tics < 50)
+                    {
+                        tics += timer1.Interval;
+                        Mixing();
+                    }
+                    else
+                    {
+                        tics = 0;
+                        button = false;
+                    }
+                    Invalidate();
+                }
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            render.DrawSprite(e.Graphics);
             render1.DrawSprite(e.Graphics);
+            render2.DrawSprite(e.Graphics);
+
+            //render11.DrawSprite(e.Graphics);
+            //render12.DrawSprite(e.Graphics);
+            //render13.DrawSprite(e.Graphics);
+            //render14.DrawSprite(e.Graphics);
+            //render15.DrawSprite(e.Graphics);
+            //render16.DrawSprite(e.Graphics);
+
+            //render21.DrawSprite(e.Graphics);
+            //render22.DrawSprite(e.Graphics);
+            //render23.DrawSprite(e.Graphics);
+            //render24.DrawSprite(e.Graphics);
+            //render25.DrawSprite(e.Graphics);
+            //render26.DrawSprite(e.Graphics);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //По нажатию вызывается метод "перемешать"
+            button = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+
+            }
         }
     }
 }
