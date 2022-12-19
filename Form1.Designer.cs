7933aad6010999a0fe0238be3af73ff1771a7657
Илья Масютин <i-masyutin@mail.ru>
2022-12-19 11:07:17 +0700

    Перевод на русский язык с английского

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,17 +46,19 @@ namespace Lab_7
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(89, 59);
+            this.label1.Location = new System.Drawing.Point(119, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 24);
+            this.label1.Size = new System.Drawing.Size(200, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Настройки игры";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(93, 276);
+            this.button1.Location = new System.Drawing.Point(124, 340);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 37);
+            this.button1.Size = new System.Drawing.Size(201, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Начать игру";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,15 +67,17 @@ namespace Lab_7
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 130);
+            this.label2.Location = new System.Drawing.Point(164, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.Size = new System.Drawing.Size(108, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Кол-во игроков";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(104, 146);
+            this.numericUpDown1.Location = new System.Drawing.Point(139, 180);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             4,
             0,
@@ -85,7 +89,7 @@ namespace Lab_7
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown1.TabIndex = 4;
             this.numericUpDown1.Value = new decimal(new int[] {
             1,
@@ -95,7 +99,8 @@ namespace Lab_7
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(104, 203);
+            this.numericUpDown2.Location = new System.Drawing.Point(139, 250);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             20,
             0,
@@ -107,7 +112,7 @@ namespace Lab_7
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown2.TabIndex = 6;
             this.numericUpDown2.Value = new decimal(new int[] {
             1,
@@ -118,27 +123,30 @@ namespace Lab_7
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(123, 187);
+            this.label3.Location = new System.Drawing.Point(164, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.Size = new System.Drawing.Size(104, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Скорость игры";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 421);
+            this.checkBox1.Location = new System.Drawing.Point(17, 518);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.Size = new System.Drawing.Size(171, 21);
             this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Develop Mode";
+            this.checkBox1.Text = "Режим тестировщика";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 319);
+            this.button2.Location = new System.Drawing.Point(124, 393);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 37);
+            this.button2.Size = new System.Drawing.Size(201, 46);
             this.button2.TabIndex = 8;
             this.button2.Text = "Загрузить игру";
             this.button2.UseVisualStyleBackColor = true;
@@ -146,9 +154,9 @@ namespace Lab_7
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 450);
+            this.ClientSize = new System.Drawing.Size(444, 554);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.numericUpDown2);
@@ -159,6 +167,7 @@ namespace Lab_7
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
